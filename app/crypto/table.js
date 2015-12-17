d805afb419a08e445372788c7a45cb337bc5c500
William Chan <williamc0206@hotmail.com>
2015-12-17 21:26:06 +0800

    update es6

--- app/crypto/table.js
+++ app/crypto/table.js
@@ -1,43 +1,42 @@
 'use strict';
-var crypto     = require('crypto');
-var util       = require('util');
-var mergeSort = require('../utils/merge-sort');
-var int32Max   = Math.pow(2, 32);
+const crypto = require('crypto');
+const util = require('util');
+const mergeSort = require('../utils/merge-sort');
+const int32Max = Math.pow(2, 32);
 
 //password: [encryptTable, decryptTable]
-var cachedTables = {};
+const cachedTables = {};
 
-var getTable = function (key) {
+const getTable = function getTable(key) {
   if (cachedTables[key]) {
     return cachedTables[key];
   }
   util.log('calculating ciphers');
 
-  var table         = new Array(256);
-  var decrypt_table = new Array(256);
-  var md5sum        = crypto.createHash('md5');
+  let table = new Array(256);
+  const decryptTable = new Array(256);
+  const md5sum = crypto.createHash('md5');
   md5sum.update(key);
-  var hash = new Buffer(md5sum.digest(), 'binary');
-  var al   = hash.readUInt32LE(0);
-  var ah   = hash.readUInt32LE(4);
-  var i    = 0;
-  while (i < 256) {
+  const hash = new Buffer(md5sum.digest(), 'binary');
+  const al = hash.readUInt32LE(0);
+  const ah = hash.readUInt32LE(4);
+
+  for (let i = 0; i < 256; i++) {
     table[i] = i;
-    i++;
   }
-  i = 1;
-  while (i < 1024) {
-    table = mergeSort(table, function (x, y) {
+
+  //TODO 想想怎么把callback拿出来
+  for (let i = 1; i < 1024; i++) {
+    table = mergeSort(table, function comparison(x, y) {  //eslint-disable-line no-loop-func
       return ((ah % (x + i)) * int32Max + al) % (x + i) - ((ah % (y + i)) * int32Max + al) % (y + i);
     });
-    i++;
   }
-  i = 0;
-  while (i < 256) {
-    decrypt_table[table[i]] = i;
-    ++i;
+
+  for (let i = 0; i < 256; i++) {
+    decryptTable[table[i]] = i;
   }
-  var result        = [table, decrypt_table];
+
+  const result = [table, decryptTable];
   cachedTables[key] = result;
   return result;
 };
