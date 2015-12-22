d39caf71e2dad9b2f72949338482b7c5205528ce
William Chan <williamc0206@hotmail.com>
2015-12-23 00:24:14 +0800

    update

--- app/crypto/openssl.js
+++ app/crypto/openssl.js
@@ -2,15 +2,7 @@
 const crypto = require('crypto');
 
 const createCipher = (key, iv, op) => {
-  const md5 = crypto.createHash('md5');
-  md5.update(key);
-  md5.update(iv);
-  const rc4Key = md5.digest();
-  if (op === 1) {
-    return crypto.createCipheriv('rc4', rc4Key, '');
-  }
 
-  return crypto.createDecipheriv('rc4', rc4Key, '');
 };
 
 exports.createCipher = createCipher;
